aed83d591a39fffcad5c1ca0a0303e2eb11e5ec5
Cillian Myles <mylsey4thewin@gmail.com>
2017-11-30 14:31:55 +0000

    Tidy up;

--- app/src/main/java/com/example/cillian/photointents/MainActivity.java
+++ app/src/main/java/com/example/cillian/photointents/MainActivity.java
@@ -5,20 +5,24 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
+/**
+ * @author Cillian Myles <mylsey4thewin@gmail.com> on 30/11/2017.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
